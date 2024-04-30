32acea60c619278fca52e75c2403ba031fc15178
fredyhg <fredyultima2@gmail.com>
2024-04-30 10:52:59 -0300

    trigger

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://18.231.160.198/api',
-  apiUrlAuth: 'http://18.231.160.198/api/auth',
+  apiUrl: 'ec2-18-231-160-198.sa-east-1.compute.amazonaws.com/api',
+  apiUrlAuth: 'ec2-18-231-160-198.sa-east-1.compute.amazonaws.com/api/auth',
 };
